52b4343e93d65ba758c07c192a30e974dfc1ea8d
aayush287 <aayushchaudhary287@gmail.com>
2023-05-11 20:55:43 +0530

    Refactor: Added cart button animation whenever item is added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,17 +1,35 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [cartHighlight, setCartHighlight] = useState(false);
   const ctx = useContext(CartContext);
 
   const totalItems = ctx.items.reduce((currNumber, item) => {
     return (currNumber += item.amount);
   }, 0);
+
+  const btnClasses = `${styles.button} ${cartHighlight ? styles.bump : ''}`;
+
+  useEffect(() => {
+    if (ctx.items.length === 0){
+      return;
+    }
+    setCartHighlight(true);
+
+    const timer = setTimeout(() => {
+      setCartHighlight(false);
+    }, 300)
+
+    return () => {
+        clearTimeout(timer)
+    }
+  }, [ctx.items])
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
