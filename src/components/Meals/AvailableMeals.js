45fc4f0591126a120e9aa49033d5f03599df51c5
aayush287 <aayushchaudhary287@gmail.com>
2023-10-21 12:22:22 +0530

    Added meals data to firestore realtime database

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,15 +1,60 @@
-import React from "react";
+import React, {useState, useEffect, useCallback } from "react";
 
 import styles from "./AvailableMeals.module.css"
-import { DUMMY_MEALS } from "../../assets/dummy-meals.js"
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 const AvaialbleMeals = props => {
-    const mealList = DUMMY_MEALS.map((meal) => <MealItem meal={meal}/>);
+    const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const fetchMealsHandler = useCallback(async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(
+          // Add db URl here
+        );
+        if (!response.ok) {
+          throw new Error("Something went wrong!");
+        }
+
+        const data = await response.json();
+
+        const loadedMeals = [];
+        for (const key in data) {
+          loadedMeals.push({
+            id: key,
+            name: data[key].name,
+            description: data[key].description,
+            price: data[key].price,
+          });
+        }
+        console.log("Testing --- " + loadedMeals)
+        setMeals(loadedMeals);
+      } catch (error) {
+        setError(error.message);
+      }
+      setIsLoading(false);
+    }, []);
+
+    useEffect(() => {
+      fetchMealsHandler();
+    }, [fetchMealsHandler]);
+
+    let content = <p>Found no meals.</p>;
+    if (isLoading) {
+      content = <p>Loading...</p>;
+    }
+    if(meals.length > 0) {
+      content = <ul>{meals.map((meal) => <MealItem key = {meal.id} meal={meal}/>)}</ul>;
+    }
+    if (error) {
+      content = <p>{error}</p>;
+    }
     return (
       <Card className={styles.meals}>
-        <ul>{mealList}</ul>
+        <section>{content}</section>
       </Card>
     );
 }
